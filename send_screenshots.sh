ce48262fab1b1a7cd7d9cd5faab889dbe4b9e2e0
André Ramos <andrel.ramos97@gmail.com>
2021-01-12 14:06:12 -0300

    Create send_screenshots.sh

---
+++ send_screenshots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Send screenshots to https://www.layoutdiff.com
+
+export IMAGEPATH=$(pwd)/screenshots/
+export PROJECTTOKEN=pLp5yB4RcQVVxsR8inOkQ4rp4qVhWvK5kcYPpTiyaopFb5NZDgxlMtMyVbSb7lonvbKPurSomq171dvyGJZA9bLvfgv7VzuBiExPK3gZwWxMc6m9eZuWU7lZnKfyQABZbVCeWY3R6P3GKxu2iKzlptcxVeDxmU5Wv0yis5yR8iY17LvuNciPzDHlOMMRnLLTwExWCI7J
+
+for filename in $IMAGEPATH/*; do
+    curl -X POST -F "image=@$filename" https://app.layoutdiff.com/images/upload/$PROJECTTOKEN/$( git log | grep -oP 'commit \K[a-f0-9]*' | head -1)
+done
