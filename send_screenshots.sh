f5f60c7ce83b612fe09c7d97b557443eb142739f
André Ramos <andrel.ramos97@gmail.com>
2021-01-12 14:47:51 -0300

    Update send_screenshots.sh

--- send_screenshots.sh
+++ send_screenshots.sh
@@ -5,9 +5,6 @@
 export IMAGEPATH=$(pwd)/screenshots/
 export PROJECTTOKEN=pLp5yB4RcQVVxsR8inOkQ4rp4qVhWvK5kcYPpTiyaopFb5NZDgxlMtMyVbSb7lonvbKPurSomq171dvyGJZA9bLvfgv7VzuBiExPK3gZwWxMc6m9eZuWU7lZnKfyQABZbVCeWY3R6P3GKxu2iKzlptcxVeDxmU5Wv0yis5yR8iY17LvuNciPzDHlOMMRnLLTwExWCI7J
 
-echo "Git log"
-git log
-
 for filename in $IMAGEPATH/*; do
     curl -X POST -F "image=@$filename" https://app.layoutdiff.com/images/upload/$PROJECTTOKEN/$GITHUB_SHA
 done
