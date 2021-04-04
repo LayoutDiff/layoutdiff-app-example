33d2f7ea3cdd5f27350881da3ef67709bc753c39
André Ramos <andrel.ramos97@gmail.com>
2021-04-04 20:18:05 -0300

    Adding a button to change background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,68 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() => runApp(LayoutDiffExample());
+
+class _BoxDecorationState extends State<LayoutDiffBoxDecoration> {
+  final List<MaterialColor> _bgColors = [Colors.blue, Colors.green, Colors.yellow, Colors.red, Colors.brown];
+  MaterialColor _bgColor = Colors.blue;
+
+  MaterialColor getRandomColor<T>() {
+    
+    final random = new Random();
+    var i = random.nextInt(_bgColors.length);
+    var color = _bgColors[i];
+
+    if(color == _bgColor) {
+      return getRandomColor();
+    }
+
+    return color;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [_bgColor[100], _bgColor[500]]
+        )
+      ),
+      child: ListView(
+          padding: const EdgeInsets.all(20),
+          children: <Widget>[
+            Center(
+              child: Text('LayoutDiff is cool!',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ElevatedButton(            
+              child: Text('Press'),
+              onPressed: () {  
+                setState(() {
+                  _bgColor = getRandomColor();
+                });
+              },
+            )
+          ]
+      )
+    );
+  }
+
+}
+
+class LayoutDiffBoxDecoration extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BoxDecorationState();
+  }
+
+}
 
 class LayoutDiffExample extends StatelessWidget {
 
@@ -12,23 +74,7 @@ class LayoutDiffExample extends StatelessWidget {
         appBar: AppBar(
           title: Text('LayoutDiff Example.'),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.yellow[100], Colors.yellow[500]]
-            )
-          ),
-          child: Center(
-            child: Text('LayoutDiff is cool!',
-              style: TextStyle(
-                fontSize: 35,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
+        body: LayoutDiffBoxDecoration()
       ),
     );
   }
