0235fa0d43a710f2591fed0214c4dbf38c3c2da6
André Ramos <andrel.ramos97@gmail.com>
2021-04-18 03:40:14 -0300

    Removing debug code

--- test_appium/example.py
+++ test_appium/example.py
@@ -20,16 +20,11 @@ desired_caps = {
     "automationName": "UiAutomator2"
 }
 
-print(desired_caps)
-
 # Instantiating the appium driver
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 driver.implicitly_wait(10)  # Set the amount of time the driver should wait when searching for elements (it's optional)
 
 # Generated test script
-
-background_color = driver.find_element_by_accessibility_id('SomeAccessibilityID').get_attribute('content-desc')
-
 el1 = driver.find_element_by_accessibility_id("Press")
 el1.click()
 
@@ -41,6 +36,3 @@ sleep(1)
 el1.click()
 sleep(1)
 el1.click()
-
-# Assertions
-text = driver.find_element_by_accessibility_id("LayoutDiff is cool!")
