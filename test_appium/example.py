3bdf7ec42cd52c652a7346c542e583e12a0db887
André Ramos <andrel.ramos97@gmail.com>
2021-06-06 04:11:58 -0300

    Crop the screenshot

--- test_appium/example.py
+++ test_appium/example.py
@@ -14,7 +14,7 @@ args=parser.parse_args()
 # Add here the desired capabitilies described on appium desktop step
 desired_caps = {
     "platformName": "Android",
-    "platformVersion": "10",
+    "platformVersion": "9",
     "deviceName": args.device_name,
     "app": args.apk_path,
     "appPackage": "com.example.layoutdiffexample",
@@ -25,42 +25,54 @@ desired_caps = {
 # Instantiating the appium driver
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
-def save_screenshot(element, screenshot_name):
+def save_screenshot(screenshot_name, element=None):
     """Save a screenshont from the tested application"""
     sleep(1)  # Wait for end of animations 
+
+    if not element:
+        # Get the root element
+        element = driver.find_elements_by_xpath("//*")[0]
+
+    # Taking screenshot
     image_path = f"{args.screenshot_path}/{screenshot_name}.png"
-    full_image = io.StringIO(base64.b64decode(driver.get_screenshot_as_base64()))
+    full_image = io.BytesIO(base64.b64decode(driver.get_screenshot_as_base64()))
     pil_image = Image.open(full_image)
-    element_x, element_y = element.location
-    element_width, element_height = element.size
-    element_crop = pil_image.crop((element_x, element_y, element_width, element_height))
+    
+    # Get element coordinates on device screen
+    element_x, element_y = element.location["x"], element.location["y"]
+    element_width, element_height = element.size["width"], element.size["height"]
+
+    # Remove OS status bar from screenshot
+    status_bar_data = driver.get_system_bars()['statusBar']
+    if status_bar_data['visible'] and element_x <= status_bar_data["x"] and element_y <= status_bar_data["y"]:
+        element_y = status_bar_data["y"] + status_bar_data["height"]
+        element_height += status_bar_data["height"]
+    
+    # Save element image
+    element_crop = pil_image.crop((element_x, element_y, element_width+element_x, element_height+element_y))
     element_crop.save(image_path)
 
 driver.implicitly_wait(10)  # Set the amount of time the driver should wait when searching for elements (it's optional)
 
 # Generated test script
-save_screenshot("home_screen")
 el1 = driver.find_element_by_accessibility_id("Press")
 el1.click()
-save_screenshot(driver.find_elements_by_xpath("/*"), "home_screen_click_blue")
-save_screenshot(el1, "press_button_blue")
+
+save_screenshot("press_button", element=el1)
+save_screenshot("home_screen_click_blue")
 
 sleep(1)
 el1.click()
-save_screenshot(driver.find_elements_by_xpath("/*"), "home_screen_click_green")
-save_screenshot(el1, "press_button_green")
+save_screenshot("home_screen_click_green")
 
 sleep(1)
 el1.click()
-save_screenshot(driver.find_elements_by_xpath("/*"), "home_screen_click_yellow")
-save_screenshot(el1, "press_button_yellow")
+save_screenshot("home_screen_click_yellow")
 
 sleep(1)
 el1.click()
-save_screenshot(driver.find_elements_by_xpath("/*"), "home_screen_click_red")
-save_screenshot(el1, "press_button_red")
+save_screenshot("home_screen_click_red")
 
 sleep(1)
 el1.click()
-save_screenshot(driver.find_elements_by_xpath("/*"), "home_screen_click_brown")
-save_screenshot(el1, "press_button_brown")
+save_screenshot("home_screen_click_brown")
